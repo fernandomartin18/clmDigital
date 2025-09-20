dbb767521bd08bce6fccf077ea7643ac899e5c42
Fer <fernandomm1840@gmail.com>
2025-09-20 14:13:40 +0200

    Arreglo de errores

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -51,7 +51,7 @@ export default function Inicio() {
           />
           <div className="inicio-info-content">
             <p>
-              Se trata de unas jornadas anuales sobre sociedad digital en Castilla-La Mancha. En esta tercera edición la temática indaga en cómo la inteligencia artificial está presente en nuestro día a día.
+              Se trata de unas jornadas anuales sobre sociedad digital en Castilla-La Mancha. En esta cuarta edición la temática indaga en cómo la inteligencia artificial está presente en nuestro día a día.
               <br />
               Tendrá una temática distinta cada año.
             </p>
@@ -68,10 +68,10 @@ export default function Inicio() {
           />
           <div className="talavera-text">
             <p>
-              CLM Digital está organizado por Eboratic, una junior empresa con sede en la Universidad de Castilla la Mancha. Las jornadas del evento se van a celebrar en Talavera de la Reina (Toledo), concretamente en la Facultad de Ciencias Sociales y Tecnologías de la Información.
+              CLM Digital está organizado por Eboratic, una junior empresa con sede en la Universidad de Castilla-La Mancha. Las jornadas del evento se van a celebrar en Talavera de la Reina (Toledo), concretamente en la Facultad de Ciencias Sociales y Tecnologías de la Información.
             </p>
             <p>
-              Talavera de la Reina, aquella que fue llamada Elbora en la antigüedad, destaca por su tradición alfarera de más de 2000 años.<br />
+              Talavera de la Reina, aquella que fue llamada Ebora en la antigüedad, destaca por su tradición alfarera de más de 2000 años.<br />
               Su cerámica se extendió a muchos de los grandes monumentos de nuestro país. Destaca también por sus iglesias de estilo mudéjar, su riqueza ornitológica a orillas del río Tajo, sus fiestas de Las Mondas (una de las más antiguas de toda Europa) y su riqueza gastronómica. Conservando su esencia ha evolucionado a una ciudad moderna, bien comunicada con el puente vertical más alto de toda España, convirtiéndose en el foco de atención de multitud de empresas del ámbito tecnológico, que ayudan a forjar futuros talentos en la ingeniería y colaborar en la investigación.
             </p>
             <p>
